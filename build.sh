948c280beb79528f1267f9510676edc26c055b02
Eric Zounes <eric.zounes@puppetlabs.com>
2016-01-04 16:14:41 -0800

    Finish implementing lambda function with config file.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp -R lib/python2.7/site-packages/* ./build
 cp lambda_function.py ./build
+cp config.json ./build
 cd build
 zip -r ../pd2jira-lambda.zip ./*
--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,40 @@ import json
 print('Loading function')
 
 
+summary_service_template = """
+Service {servicedesc} on host {host} is {state}
+"""
+summary_host_template = """
+Host/zone {host} is {state}
+"""
+description_template = """
+Add the description as needed.
+"""
+
+
 def lambda_handler(event, context):
+    with open('./config.json') as f:
+        config = json.loads(f.read())
     print("Received event: " + json.dumps(event, indent=2))
-    return event
-    #raise Exception('Something went wrong')
+    jira = JIRA(config['jira_url'], basic_auth=(config['jira_user'], config['jira_password']))
+    print(jira)
+    for message in event['messages']:
+        if message['type'] == 'incident.trigger' and message['data']['incident']['status'] == 'triggered':
+            if message['data']['incident']['trigger_summary_data']['pd_nagios_object'] == 'service':
+                issue_summary = summary_service_template.format(servicedesc=message['data']['incident']['trigger_summary_data']['SERVICEDESC'],
+                                                                host=message['data']['incident']['trigger_summary_data']['HOSTNAME'],
+                                                                state=message['data']['incident']['trigger_summary_data']['SERVICESTATE'])
+                issue_description = description_template
+
+            if message['data']['incident']['trigger_summary_data']['pd_nagios_object'] == 'host':
+                issue_summary = summary_host_template.format(servicedesc=message['data']['incident']['trigger_summary_data']['SERVICEDESC'],
+                                                             host=message['data']['incident']['trigger_summary_data']['HOSTNAME'],
+                                                             state=message['data']['incident']['trigger_summary_data']['HOSTSTATE'])
+                issue_description = description_template
+            try:
+                incident_ticket = jira.create_issue(project=config['jira_project'],
+                                                    summary=issue_summary,
+                                                    description=issue_description,
+                                                    issuetype={'name': config['jira_ticket_type']})
+            except jira.JIRAError as e:
+                print(e.text)
